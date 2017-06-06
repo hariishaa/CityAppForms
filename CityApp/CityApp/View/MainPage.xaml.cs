07b573533b9f51b773770382f709159473fe4cd4
rulezzzz08 <rulezzzz08@gmail.com>
2017-06-06 21:34:52 +0300

    First commit

---
+++ CityApp/CityApp/View/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CityApp.ViewModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CityApp.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CityApp/CityApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using CityApp.Annotations;
+using Xamarin.Forms;
+
+namespace CityApp.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _title;
+
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                _title = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Command SetTitleCommand { private set; get; }
+
+
+        public MainViewModel()
+        {
+            SetTitleCommand = new Command(SetTitle);
+        }
+
+        private void SetTitle(object title)
+        {
+            Title = title.ToString();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
