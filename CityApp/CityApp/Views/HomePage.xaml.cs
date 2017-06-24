41783939c4d5031fecfa9f3ff6755d3bd61d6815
rulezzzz08 <rulezzzz08@gmail.com>
2017-06-25 00:51:51 +0300

    Добавил страницу с записью к врачу

--- CityApp/CityApp/Views/HomePage.xaml.cs
+++ CityApp/CityApp/Views/HomePage.xaml.cs
@@ -16,5 +16,10 @@ namespace CityApp.Views
         {
             InitializeComponent();
         }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PolyclinicRegistrationPage());
+        }
     }
 }
---
+++ CityApp/CityApp/Views/PolyclinicRegistrationPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CityApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PolyclinicRegistrationPage : ContentPage
+    {
+        public PolyclinicRegistrationPage()
+        {
+            InitializeComponent();
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            await Progress.ProgressTo(0.95, 900, Easing.SinIn);
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            if (Browser.CanGoBack)
+            {
+                Browser.GoBack();
+                return true;
+            }
+            return false;
+        }
+
+        private async void Browser_Navigating(object sender, WebNavigatingEventArgs e)
+        {
+            Progress.IsVisible = true;
+            await Progress.ProgressTo(0.95, 900, Easing.SinIn);
+        }
+
+        private void Browser_Navigated(object sender, WebNavigatedEventArgs e)
+        {
+            Progress.IsVisible = false;
+            Progress.Progress = 0.15;
+        }
+    }
+}
